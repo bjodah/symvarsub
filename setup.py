b74fd6f2dca5b8b4a03ebb915aeadb6f44700a54
Björn Dahlgren <bjodah@gmail.com>
2014-01-15 12:04:10 +0100

    Bumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 name_ = 'symvarsub'
 
-version_ = '0.0.1'
+version_ = '0.0.2'
 
 if '--help'in sys.argv[1:] or sys.argv[1] in (
         '--help-commands', 'egg_info', 'clean', '--version'):
