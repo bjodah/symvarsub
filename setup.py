0e40f2f19a19021e8d8b87cab67be9201b7f6ff7
Björn Dahlgren <bjodah@gmail.com>
2016-04-17 19:59:41 +0200

    Tweaks to setup/ci

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ pkg_name = 'symvarsub'
 cmdclass = {}
 ext_modules = []
 
+def _path_under_setup(*args):
+    return os.path.join(os.path.dirname(__file__), *args)
+
+
 if len(sys.argv) > 1 and '--help' not in sys.argv[1:] and sys.argv[1] not in (
         '--help-commands', 'egg_info', 'clean', '--version'):
     from pycodeexport import pce_build_ext, PCEExtension
@@ -22,13 +26,14 @@ if len(sys.argv) > 1 and '--help' not in sys.argv[1:] and sys.argv[1] not in (
         PCEExtension(
             pkg_name + '.numtransform.transform_wrapper',
             sources=[],
-            build_files=['./symvarsub/numtransform/transform_wrapper.pyx'],
-            dist_files=[('./symvarsub/numtransform/transform_template.f90',
+            build_files=[_path_under_setup('symvarsub', 'numtransform',
+                                           'transform_wrapper.pyx')],
+            dist_files=[(_path_under_setup('symvarsub', 'numtransform', 'transform_template.f90'),
                          None)],
             build_callbacks=[
                 (
                     prebuild,
-                    ('./symvarsub/numtransform/transform_wrapper.pyx',), {}
+                    (_path_under_setup('symvarsub', 'numtransform', 'transform_wrapper.pyx'),), {}
                 )
             ],
             link_ext=False,
@@ -36,23 +41,22 @@ if len(sys.argv) > 1 and '--help' not in sys.argv[1:] and sys.argv[1] not in (
         )
     ]
 
-SYMVARSUB_RELEASE_VERSION = os.environ.get('SYMVARSUB_RELEASE_VERSION', '')
+RELEASE_VERSION = os.environ.get('SYMVARSUB_RELEASE_VERSION', '')
 
 # http://conda.pydata.org/docs/build.html#environment-variables-set-during-the-build-process
 CONDA_BUILD = os.environ.get('CONDA_BUILD', '0') == '1'
 if CONDA_BUILD:
     try:
-        SYMVARSUB_RELEASE_VERSION = 'v' + open(
+        RELEASE_VERSION = 'v' + open(
             '__conda_version__.txt', 'rt').readline().rstrip()
     except IOError:
         pass
 
 release_py_path = os.path.join(pkg_name, '_release.py')
 
-if (len(SYMVARSUB_RELEASE_VERSION) > 1 and
-   SYMVARSUB_RELEASE_VERSION[0] == 'v'):
+if len(RELEASE_VERSION) > 1 and RELEASE_VERSION[0] == 'v':
     TAGGED_RELEASE = True
-    __version__ = SYMVARSUB_RELEASE_VERSION[1:]
+    __version__ = RELEASE_VERSION[1:]
 else:
     TAGGED_RELEASE = False
     # read __version__ attribute from _release.py:
